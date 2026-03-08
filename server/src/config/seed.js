813e40fd683eae4b51990d198d7bc51b5da146ca
Michael Daza <michael.daza@csu.fullerton.edu>
2026-03-08 14:27:59 -0700

    S1-2: MongoDB and Schema + Tests

--- server/src/config/seed.js
+++ server/src/config/seed.js
@@ -5,13 +5,6 @@ export const seedDefaultAdmin = async () => {
 
     const adminUsername = process.env.ADMIN_USER || 'admin';
     const adminPassword = process.env.ADMIN_PASS || 'admin';
-/*
-    const existingAdmin = await userRepo.collection.findOne({ username: adminUsername });
-    if (existingAdmin) {
-        console.log('Admin user already exists.');
-        return;
-    }
-*/
 
     const existingAdmin = await userRepo.getUserByUsername(adminUsername);
     if (existingAdmin) {
